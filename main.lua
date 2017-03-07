b8205febf022dc21e2a3d3e9d1964c983f173a48
krishnabm <krishna.bm1995@gmail.com>
2017-03-07 12:48:30 +0530

    Revamped Title Scene added bgm, patch sprite

--- main.lua
+++ main.lua
@@ -12,6 +12,8 @@ local win = am.window{
 local world_sprite = require "sprites/env_sprites"
 -- Title Font
 local title_font = require "sprites/pup_font"    --Font name = PuppyBellies64
+-- Patch sprites
+local patch_sprite = require "sprites/patch_sprites"
 
 -----------------------------------------------
 --Scene Generator Methods
@@ -52,6 +54,9 @@ local title =
 		^am.text('Hit Space to em"bark"', vec4(0,0,0,1))
 	}
 
+local title1 = am.translate(0,250)
+			   ^am.scale(5)
+			   ^am.text("Pup\nPortal!", vec4(.7,0,0,1), "center", "top")
 --The hextiles bg node
 local env = am.group()
 --Append tiles to env
@@ -72,6 +77,10 @@ for row=8,-8,-1 do
 	end
 end	
 
+local title_pup = am.translate(135,-20)
+				  ^am.sprite(patch_sprite.patch_1_face)
+
+local rect_mask = am.rect(-400,-300,400,300,vec4(0))
 -----------------------------------------------
 --Scenes
 -----------------------------------------------
@@ -79,22 +88,39 @@ local title_scene =
 	am.group()
 	^{
 		env,
-		title
+		title,
+		title_pup
 	}
 
 local trans_scene = 
 	am.group()
 	^{
-		env
+		am.color_mask(false,false,true,false)
+		^env,
+		am.color_mask(true,false,false,false)
+		^title_pup,
+		title1,
+		rect_mask
 	}
 ---------------------------------------------------
 --Actions
 ---------------------------------------------------
+local act_arr = {}
+
+title_scene:action(am.play("assets/title.ogg",true,1,0.2))
 title_scene:action(function(scene)
 	if win:key_pressed("space") then
 		win.scene = trans_scene
 	end
 end)
+
+trans_scene:action(am.play(13416302,false,0.5,2))
+act_arr[1]= am.delay(2)
+act_arr[2]= function(scene)
+				scene"rect".color = scene"rect".color + vec4(0,0,0,0.01)
+			end
+
+trans_scene:action(am.series(act_arr))
 ---------------------------------------------------
 --Main
 ---------------------------------------------------
